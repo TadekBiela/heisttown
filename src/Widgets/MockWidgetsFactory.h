a734557899d33d9b7b1063900f2b77a8a3685601
TadekBiela <tadek.biela@gmail.com>
2023-12-13 10:22:59 +0000

    Added MockWidgetsFactory class

---
+++ src/Widgets/MockWidgetsFactory.h
@@ -0,0 +1,20 @@
+#include "StubWidget.h"
+#include "Widget.h"
+#include "WidgetGeometry.h"
+#include "WidgetType.h"
+#include "WidgetsFactory.h"
+#include <gmock/gmock.h>
+
+class MockWidgetsFactory : public WidgetsFactory
+{
+public:
+    MOCK_CONST_METHOD4(
+        create,
+        std::unique_ptr<Widget>(
+            const WidgetType& type,
+            const WidgetGeometry& geometry,
+            const WidgetText& text,
+            const WidgetStyle& style
+        )
+    );
+};
