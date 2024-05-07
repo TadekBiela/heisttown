14a006d8d22b25972db992404119282da6bc611a
TadekBiela <tadek.biela@gmail.com>
2024-05-07 20:04:39 +0000

    Added Display to LocalClient

--- src/Client/LocalClient.cpp
+++ src/Client/LocalClient.cpp
@@ -1,5 +1,12 @@
 #include "LocalClient.hpp"
 
+LocalClient::LocalClient(std::unique_ptr<Display> display)
+    : gameDisplay(std::move(display))
+{
+    gameDisplay->hide();
+}
+
 void LocalClient::run()
 {
+    gameDisplay->show();
 }
--- src/Client/LocalClient.hpp
+++ src/Client/LocalClient.hpp
@@ -2,14 +2,19 @@
 #define LOCAL_CLIENT_HPP
 
 #include "Client.hpp"
+#include <Display.hpp>
+#include <memory>
 
 class LocalClient : public Client
 {
 public:
-    LocalClient() = default;
+    LocalClient(std::unique_ptr<Display> display);
     virtual ~LocalClient() = default;
 
     void run() override;
+
+private:
+    std::unique_ptr<Display> gameDisplay;
 };
 
 #endif // LOCAL_CLIENT_HPP
